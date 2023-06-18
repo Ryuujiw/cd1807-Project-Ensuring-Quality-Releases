f288a853c9257cb8fc5f28dd3e2542e159321765
Ryuujiw <32730247+Ryuujiw@users.noreply.github.com>
2023-06-18 15:28:46 +0800

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDO2r4UuEpQI1y4jsgPrDlPoV/o97Y+qBzzcEoKcYutsqBiruM6OrKCWpbh1FICRt6V8qWHcGHv0rqai+a72kdOLoDeITBHuy2WrkkfWDjcF1zQC1p/MnFaYL/LTNbkknfrNiEelM6qmnIMkmFVLR4FWQ/gd8a4i7MWCyTEm8hh9d0FOwpliQhAiOhITPww3+GxTRamfGzdATyU56Fq+2QIbFxdqcm3HF6bd3NKihWihDmE1s1N0cG3OlKGvMmMBo9PuUWS2pRIamo/H6kuVRsKdqw5I8CnsfbNUt1MxMebdk2O5LnwtLvVo4KiXRMo4WcKYrXIU0OOkcqLgnBLPXBPJ9p+0zVJ0bTyHXgVTzEYj0F3nUDdwvKnaOa9TS1C0e6KtYSxKCFpS2V/zWAZ/3VKUX6q6CLfOOGnEN9hNqBMEDNOjKp7Lib02Is0yzVy71gIrB590zTO4qmjO842XhBsz/trKKOjWD0W99mKdaBcq8vrER1VdVKJ5SIZEsdzaFVripH92iDmVfVvMsmLlCdAiHSEWumdU8GOva08UfPzNhcgvc555nJW5XVXcbPrAeOwFdOxpZjVtGuh+w0o8rJSYJVlZvlhS1dZFnVZnPeWqr22m2olp65YKffvSB+aPgRx7T1D1LlnTCinu9ZZbGAWPFyH0GhyqrcECLudUXml8w== rujinwong@cc-9a436514-848dd567cc-9rfqz"
   }
   os_disk {
     caching           = "ReadWrite"
