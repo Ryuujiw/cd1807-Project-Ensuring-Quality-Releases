d27c536f1e3529e488298d63a53b806dbc162f0b
Ryuujiw <32730247+Ryuujiw@users.noreply.github.com>
2023-06-23 22:43:17 +0800

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -14,6 +14,8 @@ def login(user, password):
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
+    options.add_argument("--remote-debugging-port=9222")
+
     driver = webdriver.Chrome(options=options)
     print(timestamp() + 'Browser has started. Logging in...')
     driver.get('https://www.saucedemo.com/')
